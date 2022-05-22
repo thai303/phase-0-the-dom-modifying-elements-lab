6c4b3cd2fccde703721cf874a406f721a315eee9
Thai Xiong <thai303@hotmail.com>
2022-05-22 16:56:04 -0600

    commit tree 20ebf18d84dc1d5f855a9d18855eb589bfb4f2b3

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML= "Thai is the champion."
+document.body.append(newHeader)
